feb5ee58a6f3699ae7d4dd1f9d29495bb69ba743
jumaniyozovtoxir <jumaniyozovtoxir785@gmail.com>
2021-12-22 16:09:24 +0500

    chizmalarga oid kodlar jamalanmasi

--- botlar.py
+++ botlar.py
@@ -1,1 +1,14 @@
 # chizmalardan iborat kodlar jamlanmasi
+from turtle import *
+
+color('red', 'yellow')
+begin_fill()
+
+while True:
+    forward(200)
+    left(150)
+    if abs(pos()) < 1:
+        break
+
+end_fill()
+done()
